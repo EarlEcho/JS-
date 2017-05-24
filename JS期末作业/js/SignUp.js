a2b627bd3897b1216352eef6752adaacecb05cdc
EarlEcho <734952325@qq.com>
2017-05-24 08:57:36 +0800

    UpData

---
+++ JS期末作业/js/SignUp.js
@@ -0,0 +1,13 @@
+
+
+
+function check(){
+	alert("swdweuh");
+}
+function checkEmail(){
+	var userEmail = $('#userEmail').val();
+	var emailTip =$('#emailTip').html();
+	if(userEmail.length<=0){
+		emailTip='dewde';
+	}
+}
